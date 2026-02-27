04c60de2fbe6e8dcf21ef3a96e3b6b92956d9e66
Андрей Бухаров <andreybukharov@gmail.com>
2026-02-28 02:12:26 +0300

    Add test and temp

---
+++ temp_test.py
@@ -0,0 +1,24 @@
+from math import sqrt
+
+
+message = (
+    'Добро пожаловать в самую лучшую программу для вычисления '
+    'квадратного корня из заданного числа'
+    )
+print(message)
+
+
+def calculateSquareRoot(number):
+    """Вычисляет квадратный корень"""
+    return sqrt(number)
+
+
+def calc(your_number):
+    if your_number <= 0:
+        return
+    print("Мы вычислили квадратный корень из введённого вами числа. "
+          f"Это будет: {calculateSquareRoot(your_number)}")
+
+
+print(message)
+calc(25.5)
